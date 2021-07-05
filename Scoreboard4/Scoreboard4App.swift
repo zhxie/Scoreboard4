2f2344fc1b5998ac49c1c7953a50ce8138c7568a
Xie Zhihao <xzh1206@gmail.com>
2021-07-05 20:26:39 +0800

    Initial Commit

---
+++ Scoreboard4/Scoreboard4App.swift
@@ -0,0 +1,17 @@
+//
+//  Scoreboard4App.swift
+//  Scoreboard4
+//
+//  Created by 谢之皓 on 2021/7/5.
+//
+
+import SwiftUI
+
+@main
+struct Scoreboard4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
